d36d621a5dd0c3763c3f3a2850514fd1388bb430
k3lm4n <kelmandossantos@gmail.com>
2021-04-13 15:59:38 +0100

    tokens

---
+++ tokens.c
@@ -0,0 +1,79 @@
+/*PALAVRAS RESERVADAS*/
+
+const char TOKAUTO[][]={"auto","TOKAUTO"};
+const char TOKBREAK[][]={"break","TOKBREAK"};
+const char TOKCASE[][]={"case","TOKCASE"};
+const char TOKCHAR[][]={"char","TOKCHAR";
+const char TOKCONST[][]={"const","TOKCONST"};
+const char TOKCONTINUE[][]={"continue"},"OKCONTINUE";
+const char TOKDEFAULT[][]={"default","TOKDEFAULT"};
+const char TOKDO[][]={"do","OKDO"};
+const char TOKDOUBLE[][]={"double","TOKDOUBLE["};
+const char TOKELSE[][]={"else","TOKELSE"};
+const char TOKENUM[][]={"enum"},"TOKENUM";
+const char TOKEXTERN[][]={"extern","TOKEXTERN"};
+const char TOKFLOAT[][]={"float","TOKFLOAT"};
+const char TOKFOR[][]={"for"},"TOKFOR";
+const char TOKGOTO[][]={"goto","TOKGOTO"};
+const char TOKIF[][]={"if","TOKIF"};
+const char TOKINT[][]={"int","TOKINT"};
+const char TOKLONG[][]={"long","TOKLONG"};
+const char TOKREGISTER[][]={"register","TOKREGISTER"};
+const char TOKRETURN[][]={"return","TOKRETURN"};
+const char TOKSHORT[][]={"short","TOKSHORT"};
+const char TOKSIGNED[][]={"signed","TOKSIGNED"};
+const char TOKSIZEOF[][]={"sizeof","TOKSIZEOF"};
+const char TOKSTATIC[][]={"static","TOKSTATIC"};
+const char TOKSTRUCT[][]={"struct","TOKSTRUCT"};
+const char TOKSWITCH[][]={"switch","TOKSWITCH"};
+const char TOKTYPEDEF[][]={"typedef","TOKTYPEDEF"};
+const char TOKUNION[][]={"union","TOKUNION"};
+const char TOKUNSIGNED[][]={"unsigned","TOKUNSIGNED"};
+const char TOKVOID[][]={"void","TOKVOID"};
+const char TOKVOLATILE[][]={"volatile","TOKVOLATILE"};
+const char TOKWHILE[][]={"while","TOKWHILE"};
+
+/*OPERADORES ARITRIMÉTRICOS*/
+const char TOKPLUS[][]={"+","TOKPLUS"};
+const char TOKMENOS[][]={"-","TOKMENOS"};
+const char TOKDIV[][]={"/","TOKDIV"};
+const char TOKMUL[][]={"*","TOKMUL"};
+const char TOKMOD[][]={"\%","TOKMOD"};
+
+/*OPERADORES RELACIONAIS */
+
+const char TOKMENORQUE[][]={"<","TOKMENORQUE"};
+const char TOKMENIG[][]={"<=","TOKMENIG"};
+const char TOKMAIOR[][]={">","TOKMAIOR"};
+const char TOKMAOIG[][]={">=","TOKMAOIG"};
+const char TOKIGUAL[][]={"==","TOKIGUAL"};
+const char TOKDIFE[][]={"!=","TOKDIFE"};
+
+/*OPERADORES LOGICOS*/
+
+const char TOKAND[][]={"&&","TOKAND"};
+const char TOKOR[][]={"||","TOKOR"};
+const char TOKNOT[][]={"!","TOKNOT"};
+
+
+/*OPERADORES DE INC E DEC*/
+
+const char TOKINC[][]={"++","TOKINC"};
+const char TOKDEC[][]={"--","TOKDEC"};
+
+/*BITWISE*/
+
+const char TOKANDBIT[][]={"&","TOKANDBIT"};
+const char TOKORBIT[][]={"|","TOKORBIT"};
+const char TOKOREX[][]={"^","TOKOREX"};
+const char TOKDESESQ[][]={"<<","TOKDESESQ"};
+const char TOKDESDIR[][]={">>","TOKDESDIR"};
+const char TOKCOM[][]={"~","TOKCOM"};
+
+/*OPERADORES ATRIBUIÇÃO*/
+
+const char TOKATR[][]={"=","TOKATR"};
+const char TOKSOATR[][]={"+=","TOKSOATR"};
+const char TOKDECATR[][]={"-=","TOKDECATR"};
+
+
